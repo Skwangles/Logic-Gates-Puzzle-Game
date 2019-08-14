00a6c80cafbc3369c3417b2142f26dec26195747
Skwangles <skwanglesavenue@gmail.com>
2019-08-14 15:17:16 +1200

    Added an updated game Instructions Game Model with labels

--- PingPongChamps/Instructions.Designer.cs
+++ PingPongChamps/Instructions.Designer.cs
@@ -31,11 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Instructions));
             this.lblinfotxt = new System.Windows.Forms.Label();
             this.lblInfortxt2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.continueButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -55,57 +50,12 @@
             // lblInfortxt2
             // 
             this.lblInfortxt2.AutoSize = true;
-            this.lblInfortxt2.Location = new System.Drawing.Point(274, 241);
+            this.lblInfortxt2.Location = new System.Drawing.Point(374, 225);
             this.lblInfortxt2.Name = "lblInfortxt2";
             this.lblInfortxt2.Size = new System.Drawing.Size(292, 78);
             this.lblInfortxt2.TabIndex = 4;
             this.lblInfortxt2.Text = resources.GetString("lblInfortxt2.Text");
             this.lblInfortxt2.Click += new System.EventHandler(this.label4_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 158);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Move Counter";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(166, 369);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Type of Gate";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(123, 428);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Buttons";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(151, 277);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(29, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Wire";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(86, 212);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "LightBulb";
             // 
             // continueButton
             // 
@@ -120,11 +70,11 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackgroundImage = global::PingPongChamps.Properties.Resources.Napshopagain;
+            this.pictureBox1.BackgroundImage = global::PingPongChamps.Properties.Resources.GameModel;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 158);
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 129);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(256, 283);
+            this.pictureBox1.Size = new System.Drawing.Size(329, 329);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -134,11 +84,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(668, 450);
             this.Controls.Add(this.continueButton);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblInfortxt2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblinfotxt);
@@ -158,11 +103,6 @@
         private System.Windows.Forms.Label lblinfotxt;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblInfortxt2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button continueButton;
     }
 }
